43a424ad0bdc17af64d4c88b40d0fcc1d2171de4
Jayne Oliveira <oliveirajne@gmail.com>
2017-11-20 02:45:46 -0200

    add opcao configuracoes no menu

--- Origem.cpp
+++ Origem.cpp
@@ -3,18 +3,17 @@
 /*
 valor a ser recebido por venda pode ser alterado no define
 201710141736 implementada funcao para alterar a meta
+201711200230 implementada opcao no menu para configurar meta, valores e faltas permitidas
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#define var1 6
-#define var2 10
-#define var3 14
-
 int _meta = 0;
-void getmeta();
+float _valor = 0;
+int _faltas = 0;
 
+void getparametros();
 int realizado();
 int faltas();
 int calcula();
@@ -30,6 +29,7 @@ int main() {
 	printf("\n\t\tINSTRUCOES:");
 	printf("\n\t\tPressione 1 para inserir a quantidade\n\t\tde vendas realizadas pelo colaborador.\n");
 	printf("\t\tO sistema realizara o calculo e mostrara \n\t\to desempenho e valor a ser recebido.\n");
+	printf("\t\tPressione 2 para configurar parametros.\n");
 	printf("\t\tPressione 0 para sair.\n\n\n");
 	scanf("%d", &escolha);
 	fflush(stdin);
@@ -40,11 +40,11 @@ int main() {
 		case 0:
 			break;
 		case 1:
-		    getmeta();
 			desempenho();
-
+        case 2:
+            getparametros();
 		default:
-			printf("\n\t\tEntrar(1)/Sair(0)\n\n\n");
+			printf("\n\t\tEntrar(1)/Configuracoes(2)/Sair(0)\n\n\n");
 			scanf("%d", &escolha);
 			fflush(stdin);
 			continue;
@@ -57,14 +57,24 @@ int main() {
 
 }
 
-void getmeta() {
-    int metabase;
-    printf("Informe a meta base de vendas: \n");
+void getparametros() {
+    int metabase, param_faltas;
+    float param_valor;
+    printf("\n Informe a meta base de vendas: \n");
     scanf("%d", &metabase);
+    fflush(stdin);
+    printf("\n Informe o valor pago por venda: \n");
+    scanf("%f", &param_valor);
+    fflush(stdin);
+    printf("\n Informe a quantidade maxima permitida de faltas: \n");
+    scanf("%d", &param_faltas);
     fflush(stdin);
 
     _meta = metabase;
+    _valor = param_valor;
+    _faltas = param_faltas;
 
+    system("pause");
 }
 
 
@@ -106,24 +116,15 @@ void desempenho() {
 	abs = faltas();
 
     printf("\n*******************************\n");
-	if ((final >= 70) && (abs < 2)) {
-		if ((final >= 70) && (final < 100)) {
-			rv = ((final * _meta) / 100) * var1;
-			printf("\t\n\n O desempenho em relacao a meta foi de: %d por cento", final);
-			printf("\t\n Valor a ser recebido: %.2f \n\n", rv);
-		}
-		else if ((final >= 100) && (final < 130)) {
-			rv = ((final * _meta) / 100) * var2;
-			printf("\t\n\n O desempenho em relacao a meta foi de: %d por cento", final);
-			printf("\t\n Valor a ser recebido: %.2f \n\n", rv);
-		}
-		else {
-			rv = ((final * _meta) / 100) * var3;
-			printf("\t\n\n O desempenho em relacao a meta foi de: %d por cento", final);
-			printf("\t\n Valor a ser recebido: %.2f \n\n", rv);
-		}
+
+	if ((final >= 100) && (abs <= _faltas)) {
+
+        rv = ((final * _meta) / 100) * _valor;
+        printf("\t\n\n O desempenho em relacao a meta foi de: %d por cento", final);
+        printf("\t\n Valor a ser recebido: %.2f \n\n", rv);
+
 	}
-	else if ((final >= 70) && (abs >= 2)) {
+	else if ((final >= 100) && (abs > _faltas)) {
 		printf("\t\n\n O desempenho em relacao a meta foi de: %d por cento", final);
 		printf("\t\n Mas devido a quantidade de faltas, nao havera remuneracao variavel\n\n");
 	}
